ac2930d9b170128f575fa76be6d67fb7bab64ce5
Jordan Porter <tunasplam@gmail.com>
2025-11-29 22:41:20 -0800

    Autogenerating train data set

--- training/generate_train_data.py
+++ training/generate_train_data.py
@@ -1,10 +1,12 @@
-from functools import reduce
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import logging
 import json
 from math import ceil
-import operator
 import random
+from typing import get_args
 
 import numpy as np
+from tqdm import tqdm
 import uno
 
 def generate_training_data(n: int, out: str, seed: int=24, ratios: dict=None):
@@ -27,8 +29,7 @@ def generate_training_data(n: int, out: str, seed: int=24, ratios: dict=None):
 
             "play_regular_symbol": .25,
             "play_regular_color": .25,
-            "play_wild_change": .08,
-            "play_wild_same": .02,
+            "play_wild": .1,
 
             "uno_defense": .05,
             "uno_offense": .05
@@ -37,15 +38,28 @@ def generate_training_data(n: int, out: str, seed: int=24, ratios: dict=None):
     if sum(ratios.values()) != 1:
         raise ValueError("Ratios must add to 1.")
 
+    data: list[dict] = []
+
     # generate scenarios
-    data: list[dict] = reduce(
-        operator.add,
-        (generate_data_for_key(k, ceil(r*n)) for k, r in ratios.items())
-    )
+    with ProcessPoolExecutor() as executor:
+        # submit all tasks
+        futures = {
+            executor.submit(generate_data_for_key, k, ceil(r*n)): k
+            for k, r in ratios.items()
+        }
+
+        # collect results are they complete
+        for future in tqdm(
+            as_completed(futures),
+            total=len(futures),
+            desc="Generating train set data."
+        ):
+            data.extend(future.result())
 
     # TODO configure where to save training data
     with open(f"training/{out}.json", 'w', encoding="utf-8") as f:
         json.dump({"data": data[:n]}, f)
+
 
 def generate_data_for_key(k: str, n: int) -> list[dict]:
     # Check if an input key is valid. If so, map it to correct function
@@ -56,8 +70,7 @@ def generate_data_for_key(k: str, n: int) -> list[dict]:
         "draw_unneeded": draw_unneeded,
         "play_regular_symbol": play_regular_symbol,
         "play_regular_color": play_regular_color,
-        "play_wild_change": play_wild_change,
-        "play_wild_same": play_wild_same,
+        "play_wild": play_wild,
         "uno_defense": uno_defense,
         "uno_offense": uno_offense
     }
@@ -75,16 +88,35 @@ def create_input(server: uno.UnoServer, p: uno.Player) -> str:
             )
         )
 
+def random_card_no_wild() -> uno.Card:
+    "DOES NOT RETURN WILD CARDS"
+    return random.choice(get_args(uno.Color)) + random.choice(get_args(uno.Symbol))
+
+def shield_players(server: uno.UnoServer) -> uno.UnoServer:
+    for p in server.players:
+        if len(p.hand) == 1:
+            p.is_shielded = True
+    return server
+
+def shield_all_others(server: uno.UnoServer) -> uno.UnoServer:
+    "Shields all player but the active player."
+    for p in server.players:
+        if p != server.next_player and len(p.hand) == 1:
+            p.is_shielded = True
+    return server
+
+def random_card_in_hand(server: uno.UnoServer) -> str:
+    return random.randint(0,len(server.next_player.hand)-1)
+
+def playable(c: uno.Card, tc: uno.Card, next_color: uno.Color) -> bool:
+    return uno.is_wild(c) or c[0] == tc[0] or c[1] == tc[1] or c[0] == next_color
+
 # for each of these scenarios, generate a random game state
 # and then apply custom requirements.
 
 def draw_needed_forced() -> dict:
     server = random_game_state()
-
-    # if a player has one card, they must be shielded.
-    for p in server.players:
-        if len(p.hand) == 1:
-            p.is_shielded = True
+    server = shield_players(server)
 
     # set top card to be a card which forces draws
     server.deck.discard_pile[-1] = random.choice([
@@ -103,30 +135,175 @@ def draw_needed_forced() -> dict:
     }
 
 def draw_needed_no_playable() -> list[dict]:
-    return
+    server = random_game_state()
+    server = shield_players(server)
+
+    # look at top card. make sure nothing in your hand is playable.
+    top_card = server.deck.top_card_on_discard_pile()
+
+    if uno.is_wild(top_card):
+        server.next_color = random.choice(get_args(uno.Color))
+
+    server.next_player.hand = list(filter(
+        lambda c: not playable(c, top_card, server.next_color),
+        server.next_player.hand
+    ))
+
+    # what if all cards in hand were playable?
+    # generate a random cards until one is not playable. add to hand.
+    if not server.next_player.hand:
+        random_card = random_card_no_wild()
+        while playable(random_card, top_card, server.next_color):
+            random_card = random_card_no_wild()
+
+        server.next_player.hand[0] = random_card
+
+    return {
+        "input": ''.join(create_input(server, server.next_player)),
+        "output": {"action": "Draw card"}
+    }
 
 def draw_unneeded() -> list[dict]:
-    return
+    "Create a situation where there is a valid move but you draw anyway. TODO this might pollute train set..."
+    server = random_game_state()
+    server = shield_players(server)
+
+    # look at top card. make sure there is a card in your hand that is playable.
+    top_card = server.deck.top_card_on_discard_pile()
+
+    if uno.is_wild(top_card):
+        server.next_color = random.choice(get_args(uno.Color))
+
+    random_card_i = random_card_in_hand(server)
+    while not playable(server.next_player.hand[random_card_i], server.deck.top_card_on_discard_pile(), server.next_color):
+        server.next_player.hand[random_card_i] = random_card_no_wild()
+
+    server.next_player.hand = list(filter(
+        lambda c: not playable(c, top_card, server.next_color),
+        server.next_player.hand
+    ))
+
+    # what if all cards in hand were playable?
+    # generate a random cards until one is not playable. add to hand.
+    if not server.next_player.hand:
+        random_card = random_card_no_wild()
+        while playable(random_card, top_card, server.next_color):
+            random_card = random_card_no_wild()
+
+        server.next_player.hand[0] = random_card
+
+    return {
+        "input": ''.join(create_input(server, server.next_player)),
+        "output": {"action": "Draw card"}
+    }
 
 def play_regular_symbol() -> list[dict]:
-    return
+    server = random_game_state()
+    server = shield_players(server)
+
+    # make sure top card on discard is not wild.
+    top_card = server.deck.top_card_on_discard_pile()
+    while top_card[0] == "W":
+        server.deck.play(server.deck.draw())
+        top_card = server.deck.top_card_on_discard_pile()
+
+    # create a random card in hand to match symbol of top card.
+    c = random.choice(get_args(uno.Color))
+    random_card_i = random_card_in_hand(server)
+    random_card = c + top_card[1]
+    server.next_player.hand[random_card_i] = random_card
+
+    return {
+        "input": ''.join(create_input(server, server.next_player)),
+        "output": {"action": "Play card", "card": random_card}
+    }
 
 def play_regular_color() -> list[dict]:
-    return
+    server = random_game_state()
+    server = shield_players(server)
 
-def play_wild_change() -> list[dict]:
-    return
+    # make sure top card on discard is not wild.
+    top_card = server.deck.top_card_on_discard_pile()
+    while top_card[0] == "W":
+        server.deck.play(server.deck.draw())
+        top_card = server.deck.top_card_on_discard_pile()
 
-def play_wild_same() -> list[dict]:
-    return
+    # create a random card in hand to match color of top card.
+    s = random.choice(get_args(uno.Symbol))
+    random_card_i = random_card_in_hand(server)
+    random_card = top_card[0] + s
+    server.next_player.hand[random_card_i] = random_card
+
+    return {
+        "input": ''.join(create_input(server, server.next_player)),
+        "output": {"action": "Play card", "card": random_card}
+    }
+
+def play_wild() -> list[dict]:
+    server = random_game_state()
+    server = shield_players(server)
+
+    # this function applies if wild is on top so leave top_card alone.
+
+    # Force random card in hand to be a wild card
+    random_wild = random.choice(["WW", "WF"])
+    random_card_i = random_card_in_hand(server)
+    server.next_player.hand[random_card_i] = random_wild
+
+    # choose a color in hand
+    colors_in_hand = set(map(lambda c: c[0], server.next_player.hand)) - {'W'}
+    # what if they only had wild cards in hand? choose a random color
+    if not colors_in_hand:
+        c = random.choice(get_args(uno.Color))
+    else:
+        c = random.choice(list(colors_in_hand))
+
+    return {
+        "input": ''.join(create_input(server, server.next_player)),
+        "output": {"action": "Play card", "card": random_wild, "nextColor": c}
+    }
 
 def uno_defense() -> list[dict]:
-    # do when one card and unshielded
-    return
+    server = random_game_state()
+    server = shield_players(server)
+
+    # all OTHER players must be shielded.
+    server = shield_all_others(server)
+
+    # force current player to have only one card.
+    current_p = server.next_player
+    while len(current_p.hand) > 1:
+        server.deck.play(current_p.hand.pop())
+
+    return {
+        "input": ''.join(create_input(server, server.next_player)),
+        "output": {"action": "Yell UNO"}
+    }
 
 def uno_offense() -> list[dict]:
-    # do when player has one card
-    return
+    server = random_game_state()
+    server = shield_players(server)
+
+    # force a different player to have only one card.
+    random_player_i = 0
+    while server.players[random_player_i] != server.next_player:
+        random_player_i = random.randint(0,len(server.players)-1)
+
+    target_p = server.players[random_player_i]
+    while len(target_p.hand) > 1:
+        server.deck.play(target_p.hand.pop())
+
+    # all players but them must have shields
+    for p in server.players:
+        if p == target_p:
+            continue
+        if len(p.hand) == 1:
+            p.is_shielded = True
+
+    return {
+        "input": ''.join(create_input(server, server.next_player)),
+        "output": {"action": "Yell UNO"}
+    }
 
 def random_game_state() -> uno.UnoServer:
     # generate a random number of players
@@ -134,7 +311,9 @@ def random_game_state() -> uno.UnoServer:
     # agents are only used to generate relevant prompts
     s = "Do what you need to do to win"
     agents = [uno.LLMAgent(strategy=s) for _ in range(n_players)]
-    server = uno.UnoServer(agents, blank_slate=True)
+    server = uno.UnoServer(agents, blank_slate=True, log_level=logging.ERROR)
+    random_player_i = random.randint(0,len(server.players)-1)
+    server.next_player = server.players[random_player_i]
 
     # give a random number of cards to each player.
     # using poisson bc of shape and discrete data.
@@ -155,4 +334,4 @@ def random_game_state() -> uno.UnoServer:
 if __name__ == '__main__':
     # from pprint import pprint
     # pprint(generate_data_for_key("draw_needed_forced", 10))
-    generate_training_data(10, 'test')
+    generate_training_data(2000, 'autoset2k_v1')
--- uno/__init__.py
+++ uno/__init__.py
@@ -1,9 +1,16 @@
+from typing import Literal
+
 from .agents import Agent, LLMAgent, HumanAgent
-from .card import is_wild
+from .card import is_wild, Card
 from .player import Player
-from .unoserver import UnoServer
+from .unoserver import UnoServer, Color
+
+Symbol = Literal[
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "S", "R", "D"
+]
 
 __all__ = [
     "Agent", "UnoServer", "LLMAgent", "HumanAgent", "is_wild",
-    "Player"
+    "Player", "Color", "Card"
 ]
